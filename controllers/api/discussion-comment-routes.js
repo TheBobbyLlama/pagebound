c0599b3d5d37933c19e9d81d2eff2c3987917f99
alexo-a <a.ostrowski.11@gmail.com>
2020-09-12 12:38:43 -0500

    discussion topic/comment routes created

---
+++ controllers/api/discussion-comment-routes.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const { DiscussionComment, DiscussionTopic } = require('../../models');
+
+router.get('/', (req, res) => {
+    DiscussionComment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/:id', (req, res) => {
+    DiscussionComment.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/', (req, res) => {
+    // check the session
+    if (req.session) {
+        DiscussionComment.create({
+            comment_text: req.body.comment_text,
+            discussion_id: req.body.discussion_id,
+            user_id: req.session.user_id
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    DiscussionComment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No topic found with this id!' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,14 @@ const userRoutes = require('./user-routes.js');
 const clubRoutes = require('./club-routes');
 const ratingRoutes = require('./rating-routes');
 const clubSearchRoutes = require("./club-search.js");
+const discussionCommentsRoutes = require("./discussion-comment-routes");
+const discussionTopicRoutes = require("./discussion-topic-routes");
 
 router.use('/users', userRoutes);
 router.use('/clubs', clubRoutes);
 router.use('/ratings', ratingRoutes);
 router.use("/club-search", clubSearchRoutes);
+router.use("/topics", discussionTopicRoutes);
+router.use("/comments", discussionCommentsRoutes);
 
 module.exports = router;
