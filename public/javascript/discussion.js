0b2857f0c3ac2d0e6aa6f6b92f01093daef84fac
alexo-a <a.ostrowski.11@gmail.com>
2020-09-13 21:55:23 -0500

    finalized comment edit code

--- public/javascript/discussion.js
+++ public/javascript/discussion.js
@@ -4,52 +4,29 @@ async function createCommentFormHandler(event) {
     topicID = url[url.length - 1]
     if (topicID.charAt(topicID.length - 1) === "?") { topicID = topicID.slice(0, topicID.length - 1) }
     newComment = document.querySelector("#newComment").value
-    const response = await fetch('/api/comments', {
-        method: 'post',
-        body: JSON.stringify({
-            comment_text: newComment,
-            discussion_id: topicID
-        }),
-        headers: { 'Content-Type': 'application/json' }
-    })
+    if(newComment){
+        const response = await fetch('/api/comments', {
+            method: 'post',
+            body: JSON.stringify({
+                comment_text: newComment,
+                discussion_id: topicID
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        })
 
-    if (response.ok) {
-        location.reload();
-    } else {
-        alert(response.statusText);
-    }
+        if (response.ok) {
+            location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }   
 }
 
-
-
-async function createTopicFormHandler(event) {
-    event.preventDefault();
-    url = window.location.href.split("/");
-    topicID = url[url.length - 1]
-    if (topicID.charAt(topicID.length - 1) === "?") { topicID = topicID.slice(0, topicID.length - 1) }
-    newComment = document.querySelector("#newComment").value
-    const response = await fetch('/api/comments', {
-        method: 'post',
-        body: JSON.stringify({
-            comment_text: newComment,
-            discussion_id: topicID
-        }),
-        headers: { 'Content-Type': 'application/json' }
-    })
-
-    if (response.ok) {
-        location.reload();
-    } else {
-        alert(response.statusText);
-    }
+function resetComment(){
+    $("#createCommentForm").children("textarea").text("");
 }
 
-async function reloadPage(){
-    location.reload();
-}
-
-async function editComment(){
-    //console.dir($(this).attr("data-id"))
+function editComment(){
     $(this).hide()
     const commentID = $(this).attr("data-id")
     const commentContent = $(this).parent().parent().children("p")
@@ -61,15 +38,12 @@ async function editComment(){
         .val(text);
     commentContent.replaceWith(textInput);
     textInput.trigger("focus");
-
-    
     textInput.after(`<button id="cancelBtn" class='button cancel'>Cancel</button>`)
     textInput.after(`<button id='${commentID}' class='button save'>Save</button>`)
 }
 
-async function abortEdit(){
-    console.log("aborted")
-
+function abortEdit(){
+    location.reload();
 }
 
 async function saveComment(){
@@ -79,9 +53,7 @@ async function saveComment(){
         topicID = topicID.slice(0, topicID.length - 1) 
     }
     const commentId = $(this).attr("id")
-    console.log("saved")
     $("#cancelBtn").remove();
-    console.dir($(this).siblings(".button"))
     const commentContent = $(this).parent().children("textarea")
     $(this).remove();
     const text = commentContent
@@ -115,5 +87,4 @@ $(document).on('click', 'button[data-id]', editComment)
 $(document).on('click', '.save', saveComment)
 $(document).on('click', '.cancel', abortEdit)
 document.querySelector('#createCommentForm').addEventListener('submit', createCommentFormHandler);
-//document.querySelector('#createTopicForm').addEventListener('submit', createTopicFormHandler);
-document.querySelector('#createCommentForm').addEventListener('reset', reloadPage);
+document.querySelector('#createCommentForm').addEventListener('reset', resetComment);
